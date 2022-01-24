48599276a4f73594a4e5e1c319004e7aca58f489
vitaly <vitalyshaparau@gmail.com>
2022-01-24 22:36:04 +0300

    parser-7-build xslx

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2022_01_23_171836) do
 
-ActiveRecord::Schema.define(version: 2022_01_21_192703) do
-
-  create_table "brand_infos", charset: "latin1", force: :cascade do |t|
+  create_table "brand_infos", charset: "utf8", force: :cascade do |t|
     t.string "brand_name"
     t.string "brandid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-
-  create_table "data_ratings", charset: "latin1", force: :cascade do |t|
+  create_table "data_ratings", charset: "utf8", force: :cascade do |t|
     t.bigint "part_id"
     t.json "data"
     t.json "currency"
@@ -40,7 +38,7 @@ ActiveRecord::Schema.define(version: 2022_01_21_192703) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", charset: "latin1", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
