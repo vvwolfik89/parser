02011376d51fa10679c0866c3d6b5a1835aecba4
vitaly <vitalyshaparau@gmail.com>
2022-01-25 21:33:54 +0300

    parser-8-build proxy

--- app/models/data_rating.rb
+++ app/models/data_rating.rb
@@ -7,7 +7,7 @@ class DataRating < ApplicationRecord
   protected
 
   def check_date_uniq
-    date_time = DateTime.now
+    date_time = DateTime.current
     date_range = (date_time.beginning_of_day .. date_time.end_of_day)
     unless DataRating.where(created_at: date_range).where(part_id: self.part_id).empty?
       errors.add(:base, "This is part already have rating for current day")
