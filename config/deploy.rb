ce5b9116522547f5a4c449d7af0cb8a3cd03b8fa
vitaly <vitalyshaparau@gmail.com>
2022-01-30 21:30:50 +0300

    parser-12-files for deploy 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,6 +50,89 @@ namespace :git do
   end
 end
 
+
+namespace :deploy do
+  desc 'Setup'
+  task :setup do
+    on roles(:all) do
+      execute "mkdir  #{shared_path}/config/"
+      execute "mkdir  /var/www/apps/#{application}/run/"
+      execute "mkdir  /var/www/apps/#{application}/log/"
+      execute "mkdir  /var/www/apps/#{application}/socket/"
+      execute "mkdir #{shared_path}/system"
+      sudo "ln -s /var/log/upstart /var/www/log/upstart"
+
+      upload!('shared/database.yml', "#{shared_path}/config/database.yml")
+
+      upload!('shared/Procfile', "#{shared_path}/Procfile")
+
+
+      upload!('shared/nginx.conf', "#{shared_path}/nginx.conf")
+      sudo 'stop nginx'
+      sudo "rm -f /usr/local/nginx/conf/nginx.conf"
+      sudo "ln -s #{shared_path}/nginx.conf /usr/local/nginx/conf/nginx.conf"
+      sudo 'start nginx'
+
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:create"
+        end
+      end
+
+
+
+    end
+  end
+
+  desc 'Create symlink'
+  task :symlink do
+    on roles(:all) do
+      execute "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+      execute "ln -s #{shared_path}/Procfile #{release_path}/Procfile"
+      execute "ln -s #{shared_path}/system #{release_path}/public/system"
+    end
+  end
+
+  desc 'Foreman init'
+  task :foreman_init do
+    on roles(:all) do
+      foreman_temp = "/var/www/tmp/foreman"
+      execute  "mkdir -p #{foreman_temp}"
+      # Создаем папку current для того, чтобы foreman создавал upstart файлы с правильными путями
+      execute "ln -s #{release_path} #{current_path}"
+
+      within current_path do
+        execute "cd #{current_path}"
+        execute :bundle, "exec foreman export upstart #{foreman_temp} -a #{application} -u deployer -l /var/www/apps/#{application}/log -d #{current_path}"
+      end
+      sudo "mv #{foreman_temp}/* /etc/init/"
+      sudo "rm -r #{foreman_temp}"
+    end
+  end
+
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      sudo "restart #{application}"
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+  after :finishing, 'deploy:restart'
+
+  after :updating, 'deploy:symlink'
+
+  after :setup, 'deploy:foreman_init'
+
+  after :foreman_init, 'foreman:start'
+
+  before :foreman_init, 'rvm:hook'
+
+  before :setup, 'deploy:starting'
+  before :setup, 'deploy:updating'
+  before :setup, 'bundler:install'
+end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
