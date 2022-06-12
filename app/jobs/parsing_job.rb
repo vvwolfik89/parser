c3e677fb30912adcee55fad10db75e13665b3c56
vitaly <vitalyshaparau@gmail.com>
2022-06-13 00:15:55 +0300

    feature-17-rebuild parsing job

--- app/jobs/parsing_job.rb
+++ app/jobs/parsing_job.rb
@@ -1,13 +1,9 @@
 class ParsingJob
   include Sidekiq::Job
 
-  def perform
-    date_time = DateTime.current
-    date_range = (date_time.beginning_of_day .. date_time.end_of_day)
-    parts = Part.without_current_data_ratings(date_range).uniq
-    parts.each do |part|
-      parser_data = ParserService.new(part)
-      parser_data.show_data
-    end
+  def perform(part_id)
+    part = Part.find(part_id)
+    parser_data = ParserService.new(part)
+    parser_data.show_data
   end
 end
